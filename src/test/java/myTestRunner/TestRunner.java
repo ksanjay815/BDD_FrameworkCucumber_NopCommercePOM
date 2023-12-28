0aaf950043f8661ff8efd8a4a4c72f31958a9eee
sanjay kumar <sanjaykumar,marolix@gmail.com>
2023-12-29 05:28:24 +0530

    29 dec

--- src/test/java/myTestRunner/TestRunner.java
+++ src/test/java/myTestRunner/TestRunner.java
@@ -7,20 +7,28 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//		features="Features",   // it will select all feature files 
-		features=".\\Features\\Login.feature",    //it will selec the selected feature file
+		features="Features",   // it will select all feature files 
+//		features=".\\Features\\Login.feature",    //it will selec the selected feature file
 //		features=".\\Features\\Customer.feature",
+		
+		
 		glue="StepDefinitions",
-		dryRun=false,
-		monochrome =true,
-		plugin= {"pretty","html:target/HtmlReports"}
-	
-//		,"json:target/cucumber.json"
-//		tags= "@regression"
-//		tags= "@sanity"
-//		tags= "@smoke"
+		plugin= { "pretty","html:reports/myreport.html", 
+				  "json:target/cucumber.json",
+				  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				  "rerun:target/rerun.txt",
+				},
+				
+		dryRun=false,    // checks mapping between scenario steps and step definition methods
+		monochrome=true,    // to avoid junk characters in output
+		publish=true  // to publish report in cucumber server
+		//tags="@sanity"  // this will execute scenarios tagged with @sanity
+		//tags="@regression"
+		//tags="@sanity and @regression" //Scenarios tagged with both @sanity and @regression
+		//tags="@sanity and not @regression" //Scenarios tagged with @sanity but not tagged with @regression
+		//tags="@sanity or @regression" //Scenarios tagged with either @sanity or @regression
 //		tags= "~@end2end"   //this symbol will ignore the end2end testcases
-//		"tags",{"@smoke","@sanity"}  // this step will execute the two testcases
+		//"tags",{"@smoke","@sanity"}  // this step will execute the two testcases
 		)
 public class TestRunner {
 
